d48a3c43f4490cc4376aa6871574dfe4ebacc50c
Johni Douglas Marangon <johni.marangon@softplan.com.br>
2025-06-04 20:26:31 -0300

    Refactor project

--- extract-data-from-pdf/src/main/java/com/example/extract_data_from_pdf/controller/RootController.java
+++ extract-data-from-pdf/src/main/java/com/example/extract_data_from_pdf/controller/RootController.java
@@ -1,11 +1,19 @@
 package com.example.extract_data_from_pdf.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
@@ -13,7 +21,7 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.ai.content.Media;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
@@ -33,7 +41,6 @@ public class RootController {
     public RootController(ChatModel chatModel, ChatClient.Builder chatClientBuilder) {
         this.chatModel = chatModel;
         this.chatClient = chatClientBuilder.build();
-
     }
 
     @GetMapping("/")
@@ -41,74 +48,56 @@ public class RootController {
         return "Welcome to PDF Data Extraction API";
     }
 
-    // @PostMapping(value="/carCounts", consumes =
-    // MediaType.MULTIPART_FORM_DATA_VALUE, produces =
-    // MediaType.APPLICATION_JSON_VALUE)
-    // public carCounts(
-    // @Parameter(description = "File to be uploaded", required = true)
-    // @RequestParam("file") MultipartFile file, @RequestParam("colors") String
-    // colors) {
-    // try (InputStream inputStream = file.getInputStream()) {
-    // var carCount = carCountService.countCar(inputStream, file.getContentType(),
-    // colors);
-    // return ResponseEntity.ok(carCount);
-    // } catch (IOException e) {
-    // return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error
-    // uploading file");
-    // }
-    // }
-
     @PostMapping("/ner")
     public ResponseEntity<?> ner(@RequestParam("file") MultipartFile file) throws FileNotFoundException {
 
         String systemText = """
-                Você é um assistente que extrai dados específicos de boletos e documentos para pagamento.
+        Você é um assistente especializado na extração de informações estruturadas a partir de documentos financeiros brasileiros, especialmente boletos bancários.
+        Seu objetivo é analisar a(s) imagem(s) e extrair os principais campos.
+        """;
 
-                Você receberá o conteúdo de um documento a ser pago.
+        String userText = """
+        Boletos:
+        """;
 
-                Regras:
-                - As datas devem ser retornadas no formato **dd/mm/aaaa**.
-                - O **sacado** é a pessoa ou empresa que deve pagar o título.
-                - **CNPJ** são válidos com 14 dígitos numéricos. Exemplo: 34.877.404/0001-80
-                - **CPF** são válidos com 11 dígitos numéricos. Exemplo: 100.993.400-70
-                - Formato inválido de CNPJ: 1840969662/2025 (Documento de Arrecadação Municipal - D.A.M.)
-                - Formato inválido de CNPJ: 83690000001-6 (chunk do código de barras)
-                - **Não gere números para campos que não foram encontrados no documento.**
-                - Se não conseguir extrair os dados, retorne o campo como **string vazia** (`""`), ou `0.0` no caso de valores numéricos.
 
-                Retorne os dados extraídos em formato **JSON**, seguindo o schema abaixo:
-
-                ```json
-                {
-                "cnpj_beneficiario": "string",
-                "nome_beneficiario": "string",
-                "cpf_cnpj_empresa_sacado": "string",
-                "nome_empresa_sacado": "string",
-                "numero_documento": "string",
-                "data_documento": "string",
-                "data_emissao": "string",
-                "valor_total_documento": 0.0,
-                "valor_desconto": 0.0,
-                "data_vencimento": "string"
-                }
-
-                """;
+        List<Media> images = new ArrayList<>();
 
         try (InputStream inputStream = file.getInputStream()) {
+            PDDocument document = PDDocument.load(inputStream);
+            PDFRenderer pdfRenderer = new PDFRenderer(document);
+
+            for (int page = 0; page < document.getNumberOfPages(); ++page) {
+                BufferedImage image = pdfRenderer.renderImageWithDPI(page, 300); // 300 DPI gives high quality
+                String pathname = "/home/johni.marangon@softplan.com.br/Desktop/output_page_" + (page + 1) + ".png";
+                ImageIO.write(image, "png", new File(pathname));
+            }
+            document.close();
+
+            for (int page = 0; page < document.getNumberOfPages(); ++page) {
+                String pathname = "/home/johni.marangon@softplan.com.br/Desktop/output_page_" + (page + 1) + ".png";
+
+                File imageFile = new File(pathname);
+                byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
+
+                images.add(
+                        Media.builder().id("page" + page + 1)
+                                .mimeType(MimeTypeUtils.IMAGE_PNG)
+                                .data(imageBytes)
+                                .build());
+
+            }
+
+            UserMessage u = UserMessage.builder().text(userText).media(images).build();
             String response = chatClient.prompt()
                     .system(systemText)
-
-                    .user(userMessage -> userMessage
-                            .text("Boleto de pagamento:")
-                            .media(MimeTypeUtils.parseMimeType(file.getContentType()),
-                                    new InputStreamResource(inputStream)))
+                    .messages(u)
                     .call().content();
 
             return ResponseEntity.ok(response);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading image");
         }
-
     }
 
     @GetMapping("/chat")
